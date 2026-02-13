a88b21be25881182bfb29ff8e9d70c7a0f37b8fe
Harikrishnan Santhosh <harikrishnansanthosh06@gmail.com>
2026-02-13 09:50:10 +0530

    Update

--- src/CustomException.java
+++ src/CustomException.java
@@ -13,13 +13,13 @@ public class CustomException {
 
         try {
             if (age < 18) {
-                throw new InvalidAgeException("Not eligible to vote");
+                throw new InvalidAgeException("Exception: Not eligible to vote");
             } else {
                 System.out.println("Eligible to vote");
             }
         } 
         catch (InvalidAgeException e) {
-            System.out.println("Exception: " + e.getMessage());
+            System.out.println(e.getMessage());
         }
 
         sc.close();
